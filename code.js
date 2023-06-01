368f1c6a649fc6c67a84e428189c69cc58dc3a79
Wainsix <wainsix@gmail.com>
2023-06-01 10:36:48 +0300

    tehtäviä

---
+++ code.js
@@ -0,0 +1,50 @@
+/*
+function sayHello() {
+    console.log("BAD = browser application development");
+}
+function Tarkista(){
+var arvo = document.getElementById("tekstikentta").value;
+console.log("Tekstikentän arvo oli " + arvo);
+}
+
+
+document.getElementById("teksti").innerHTML = "JavaScript on helppoa ja hauskaa!";
+*/
+/*
+function laskeNelio (){
+    var numElement = document.getElementById("numero");
+    var num = parseInt(numElement.textContent);
+   var nelio = num*num
+   console.log("Luvun " + num + " neliö on " + nelio)
+}
+*/
+/*
+
+function tervehdi() {
+
+    console.log("Moikka!");
+
+}
+
+function lisaaPainike(){
+// Create the button element
+var button = document.createElement("button");
+
+// Set the button properties and attributes
+button.innerHTML = "Click me";
+button.id = "luotuPainike";
+button.setAttribute("onclick", "tervehdi()")
+// Append the button to the document body
+document.body.appendChild(button);
+
+}
+*/
+
+/*
+function laskeNelio() {
+    var numElement = document.getElementById("luku");
+    var num = parseInt(numElement.value);
+    var nelio = num * num;
+    document.getElementById("tulos").innerHTML = "Luvun " + num + " neliö on " + nelio;
+}
+*/
